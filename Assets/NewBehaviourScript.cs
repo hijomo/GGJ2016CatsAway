eee59590647bd89616424a5777e886f515cf184d
hijomo <hijomo@gmail.com>
2016-01-30 09:26:48 -0700

    start of political scene

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	string positiveText;
+	string negativeText;
+	string nutralText;
+
+	/*
+	 * what it the idea,
+	 * whne a button is pressed it will increment the camera view one, and addto the crownd felling
+	 */
+
+	public void SpeechBoxAPress ()
+	{
+		Debug.Log ("Button A has been Pressed");
+	}
+
+	public void SpeechBoxBPress ()
+	{
+		Debug.Log ("Button B has been Pressed");
+	}
+
+	public void SpeechBoxCPress ()
+	{
+		Debug.Log ("Button C has been Pressed");
+	}
+}
