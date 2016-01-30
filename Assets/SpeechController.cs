3d5e2d0f6f276d83058e0baba43075f162fb76f1
hijomo <hijomo@gmail.com>
2016-01-30 10:26:51 -0700

    changed controller script name

--- Assets/SpeechController.cs
+++ Assets/SpeechController.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SpeechController : MonoBehaviour
 {
+
 	string positiveText;
 	string negativeText;
 	string nutralText;
 
 	/*
 	 * what it the idea,
-	 * whne a button is pressed it will increment the camera view one, and addto the crownd felling
+	 * whne a button is pressed it will increment the camera view one
+	 * , and add to the crownd felling based on what is sellected
+	 * 
 	 */
 
 	public void SpeechBoxAPress ()
